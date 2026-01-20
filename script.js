51717a931db2f8037a0ee4a607ef2bfc6c49dd84
diwakarns <diwakarbarbigha@gmail.com>
2026-01-20 23:07:25 +0530

    Add static website files

---
+++ script.js
@@ -0,0 +1,9 @@
+// --- Mobile Navigation Toggle ---
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+if (hamburger) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
